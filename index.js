35829d029cb67309137753d949ec173fac8eb0fa
dimaBel1 <dima.chunosov86@mail.ru>
2023-08-14 14:01:08 +0300

    3333333

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const pg = require('pg');
+require('dotenv').config();
 
 
 
 const app = express();
 
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    // ssl: true
+})
+
 app.get('/', (req, res) => {
     res.send('hello 11112222221')
 })
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
+})
+
 
 
 app.listen(3000);
